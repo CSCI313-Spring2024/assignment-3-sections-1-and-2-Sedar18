fffbf515db16340efd903f2c8f121d4899021d0d
Sedar18 <sedar.hebib@ndsu.edu>
2025-03-19 01:39:05 -0500

    integrated details page into the app

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,14 +1,18 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { DetailsComponent } from './details/details.component';
+
+
 export const routes: Routes = [
 {
 path: '',
 component: HomeComponent,
 title: 'Home page',
 },
+
+
 {
-path: 'details',
+path: 'details/:id',
 component: DetailsComponent,
 title: 'Home Details'
 },
